62d96360752f74569d75e20b0e1a4441cf5ad965
Mahedi Hasan <118504951+Hasan9927@users.noreply.github.com>
2024-01-05 04:46:37 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = '33'
+let valueInNumber = Number(score)
+
+//"33" =>33
+// ture => 1
+// false => 0
+// "33abc" => NaN
+
+let isLoggedIn = true;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0 =>false
+// "" =>false;
+//"Name" =>true;
