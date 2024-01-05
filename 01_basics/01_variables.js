35025269baf18efe0db809ab22feedaa1d0957a5
Mahedi Hasan <118504951+Hasan9927@users.noreply.github.com>
2024-01-05 04:18:29 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 2233;
+let name = "" ;
+let country =  "Bangladesh";
+/*
+Prefer not to use var
+Because of issue in block scpoe and function scope
+*/
+console.table([name= "Hasan", accountId, country])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as newer version
+
+// number
+// bigint => if the number is more than 2 to the power 53
+// string = ""
+// boolean => true or false
+// null => standalone value
+// undefined => the value is not assigned
+// symbol => unique
+
+
+// object => null is also object; [iterview questions]
+//null
+console.log("typeof null is "+ typeof null);
+console.log(typeof undefined);
