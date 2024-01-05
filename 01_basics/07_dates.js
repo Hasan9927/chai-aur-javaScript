ee4e2eda77c9d10e4ab8fdc6e5b8b0dbb657f7d6
Mahedi Hasan <118504951+Hasan9927@users.noreply.github.com>
2024-01-05 11:30:35 +0000

    added time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+// Dates
+
+let myDate = new Date();
+console.log(typeof myDate);//object
+console.log(myDate.toString());
+console.log(myDate.toDateString());//gives date only
+console.log(myDate.toTimeString());//gives time only
+console.log(myDate.toLocaleDateString());//give the local date
+console.log(myDate.toLocaleString());//gives local time and date
+console.log(myDate.toLocaleTimeString());//gives locan time only
+
