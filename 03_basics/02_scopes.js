34501577e4276643085f0cd02424facb683f70be
Mahedi Hasan <118504951+Hasan9927@users.noreply.github.com>
2024-01-06 10:19:12 +0000

    learnt little bit of scope and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -14,3 +14,25 @@ if(true){
 // console.log(b); // It will give an error b is not defined
 console.log(c); // but it doesn't show any error cz var is not maintain scope resulation method soo we avoid it.
 
+
+if(true){
+    const username = 'Hasan';
+    if(true){
+        const website = "youtube";
+        console.log(username, website);// can access username cz it in the global scope
+    }
+    // console.log(website); //can't clg cz it is outsite of the scope
+}
+
+// we can call funtion before it declared but ....
+addOne(2);
+function addOne(num){
+    return num+1;
+}
+// but .. here we will face error cz we keep the function inside in a variable which against the hoistin rules, so we have to keep it below where we declare it
+
+// addTwo(2);
+const addTwo = function(num){
+    return num+2;
+}
+addTwo(2);
