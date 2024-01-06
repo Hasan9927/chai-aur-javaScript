cb6872d164e8fb0ecedbc41bcc91f4a130220e1a
Mahedi Hasan <118504951+Hasan9927@users.noreply.github.com>
2024-01-06 09:29:34 +0000

    Function done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,7 +20,32 @@ function loginUserMessage(username){
     if(!username || (username.trim()).length == 0){
         return console.log('Please enter a username')
     }
-    return `${username.trimStart().trimEnd()} just logged in.`
+    return `"${username.trimStart().trimEnd()}" just logged in.`
 };
-const userStatus = loginUserMessage("       a      ");
+const userStatus = loginUserMessage(" Mahedi Hasan");
 console.log(userStatus);
+
+/// (...) is called rest and spread operator both based on their use case 
+// Example
+
+function addedItems(...items){
+    return items;
+}
+console.log(addedItems(100,200));
+// Object
+const user = {
+    userName: "Hasan",
+    password: "123pass",
+}
+function handleObject(anyobject){
+    console.log(`User name is ${anyobject.userName} and password is ${anyobject.password}`);
+}
+handleObject(user);
+
+// Array
+
+const newArray  = [12,15,154,15785];
+function getNewArray(array){
+    return array[1];
+}
+console.log(getNewArray(newArray));
