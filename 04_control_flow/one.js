a8adf56599bd5eaba2fd49999871c9d4699f9377
Mahedi Hasan <118504951+Hasan9927@users.noreply.github.com>
2024-01-07 05:59:24 +0000

    if else statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+// Shrothand Notation
+const balance = 500;
+if(balance >400) console.log("Test Completed"); // Implicite scope
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy courses");
+}
