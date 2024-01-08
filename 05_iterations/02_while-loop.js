409556655fd35184225cdf3bc77ac9d9b50775f1
Mahedi Hasan <118504951+Hasan9927@users.noreply.github.com>
2024-01-08 06:13:25 +0000

    While and do-while loop completed

---
+++ 05_iterations/02_while-loop.js
@@ -0,0 +1,21 @@
+// While
+let index = 0;
+while (index <=10) {
+    // console.log(`Value of index is: ${index}`);
+    index+=2;
+}
+
+let myBrothers = ["Hossain","Hasib", "Tahomid", "Tasnimul"];
+let arr = 0;
+while (arr < myBrothers.length){
+    console.log(`My ${arr+1}th brother is: ${myBrothers[arr]}`);
+    arr++;
+}
+
+
+// Do while loop
+let score = 11;
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10);
