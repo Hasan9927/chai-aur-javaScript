b378c8a04aef785349f01b602a89f452ec73bb0d
Mahedi Hasan <118504951+Hasan9927@users.noreply.github.com>
2024-01-25 15:37:17 +0000

    objects

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,3 +1,27 @@
 const user = {
-    
+    userName: 'Hasan',
+    loginCount: 5,
+    signedIn: true,
+
+    getUserDetails: function(){
+        console.log(`UserName: ${this.userName}`)
+    }
 }
+
+console.log(user.getUserDetails());
+
+// Importance of "new"
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this;
+}
+
+const userOne = new User('Hasan', 12, true);
+const userTwo = new  User('Mahedi', 20, false)
+
+console.log(userOne);
+console.log(userTwo);
